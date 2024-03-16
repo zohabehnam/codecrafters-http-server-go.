a70347ae50ca19d21bef5f75451e8443ad14a32c
ztierra <hemiola21@gmail.com>
2024-03-16 15:44:53 +0330

    feat(app): handle multiple clients

--- app/server.go
+++ app/server.go
@@ -6,19 +6,23 @@ import (
 	"net"
 	"os"
 	"strings"
-	"log"
 	"flag"
+	"bytes"
+	"errors"
+	"io"
 )
+
+var dirFlag *string
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-	directory := flag.String("directory", "", "path to server directory")
+	dirFlag = flag.String("directory", "", "Specify directory")
 	flag.Parse()
 
 	// Uncomment this block to pass the first stage
 	//
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	l, err := net.Listen("tcp", "localhost:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
@@ -27,86 +31,104 @@ func main() {
 	defer l.Close()
 	for {
 		conn, err := l.Accept()
+		defer conn.Close()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
-		go handleConnection(conn, *directory)
+		go handleConnection(conn)
 	}
-	// buffer := make([]byte, 1024)
-	// _, err = conn.Read(buffer)
-	// if err != nil {
-	// 	fmt.Println("Error reading", err)
-	// 	return
-	// }
-	// req := string(buffer)
-	// firstLine := strings.Split(req, "\r\n")[0]
-	// path := strings.Fields(firstLine)[1]
-	// if path == "/" {
-	// 	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	// } else if strings.HasPrefix(path, "/echo") {
-	// 	body := strings.SplitAfterN(path, "/", 3)
-	// 	content := body[len(body)-1]
-	// 	conn.Write([]byte(fmtResponseContent(content)))
-	// } else if strings.HasPrefix(path, "/user-agent") {
-	// 	thirdLine := strings.Split(req, "\r\n")[2]
-	// 	userAgent := strings.TrimSpace(strings.Split(thirdLine, ":")[1])
-	// 	conn.Write([]byte(fmtResponseContent(userAgent)))
-	// } else {
-	// 	conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
-	// }
 }
 
-func handleConnection(conn net.Conn, directory string)  {
-	defer conn.Close()
-	buffer := make([]byte, 1024)
-	_, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading", err)
-		return
-	}
-	req := string(buffer)
-	// log.Printf("Request: %s", req)
-	firstLine := strings.Split(req, "\r\n")[0]
-	method := strings.Fields(firstLine)[0]
-	path := strings.Fields(firstLine)[1]
-	log.Println(req)
-	if path == "/" {
-		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	} else if strings.HasPrefix(path, "/echo") {
-		body := strings.SplitAfterN(path, "/", 3)
-		content := body[len(body)-1]
-		conn.Write([]byte(fmtResponseContent(content)))
-	} else if strings.HasPrefix(path, "/user-agent") {
-		// body := strings.SplitAfterN(path, "/", 3)
-		// content := body[len(body)-1]
-		thirdLine := strings.Split(req, "\r\n")[2]
-		userAgent := strings.TrimSpace(strings.Split(thirdLine, ":")[1])
-		conn.Write([]byte(fmtResponseContent(userAgent)))
-	}  else if strings.HasPrefix(path, "/files") && method == "GET" {
-		body := strings.SplitAfterN(path, "/", 3)
-		fileName := body[len(body)-1]
-		bytes, err := os.ReadFile(fmt.Sprintf("%s/%s", directory, fileName))
-		if err != nil {
-			conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
-			return
+func handleConnection(conn net.Conn) {
+	for {
+		req := make([]byte, 1024)
+		_, err := conn.Read(req)
+		if errors.Is(err, io.EOF) {
+			fmt.Println("Client broke the connection")
+			break
+		} else if err != nil {
+			fmt.Println("Error reading the request")
+			break
 		}
-		conn.Write([]byte(fmtFileResponse(string(bytes))))
-	} else {
-		conn.Write([]byte("HTTP/1.1 404 NOT FOUND\r\n\r\n"))
+		content := strings.Split(string(req), "\r\n")
+		if len(content) == 0 {
+			continue
+		}
+		requestLine := content[0]
+		data := strings.Split(requestLine, " ")
+		if len(data) < 2 {
+			continue
+		}
+		method := data[0]
+		path := data[1]
+		if strings.HasPrefix(path, "/files") {
+			if method == "POST" {
+				handlePostFiles(path, content[len(content)-1], conn)
+			} else {
+				handleGetFiles(path, conn)
+			}
+		} else if strings.HasPrefix(path, "/user-agent") {
+			handleUserAgent(content, conn)
+		} else if strings.HasPrefix(path, "/echo/") {
+			handleEcho(path, conn)
+		} else if path != "/" {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"))
+		} else {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		}
 	}
+}
+
+func handleEcho(path string, conn net.Conn) {
+	body := strings.SplitAfterN(path, "/", 3)
+	content := body[len(body)-1]
+	conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(content), content)))
 }
 
 func fmtResponseContent(content string) string {
 	return fmt.Sprint(fmtResponse(content, "text/plain") + content)
 }
-func fmtFileResponse(content string) string {
-	return fmt.Sprint(fmtResponse(content, "application/octet-stream") + content)
+
+func handlePostFiles(path string, content string, conn net.Conn) {
+	dir := *dirFlag
+	arrPath := strings.SplitAfterN(path, "/", 3)
+	finalPath := arrPath[len(arrPath)-1]
+	err := os.WriteFile(dir+"/"+finalPath, bytes.Trim([]byte(content), "\x00"), 0666)
+	if err != nil {
+		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\nContent-Length: 0\r\n\r\n"))
+		return
+	}
+	conn.Write([]byte("HTTP/1.1 201 OK\r\nContent-Length: 0\r\n\r\n"))
 }
+
 
 func fmtResponse(content string, contentType string) string {
 	return fmt.Sprintf(
 		"HTTP/1.1 200 OK\r\nContent-Type: %s\r\nContent-Length: %d\r\n\r\n",
 		contentType,
 		len(content))
+}
+
+func handleGetFiles(path string, conn net.Conn) {
+	dir := *dirFlag
+	body := strings.SplitAfterN(path, "/", 3)
+	pathToFile := body[len(body)-1]
+	file, err := os.ReadFile(dir + "/" + pathToFile)
+	if err != nil {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n"))
+		return
+	}
+	conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(file), file)))
+}
+func handleUserAgent(request []string, conn net.Conn) {
+	content := ""
+	for _, line := range request {
+		if strings.Contains(line, "User-Agent") {
+			fmt.Println(line)
+			content = strings.Split(line, ": ")[1]
+			break
+		}
+	}
+	conn.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(content), content)))
 }
